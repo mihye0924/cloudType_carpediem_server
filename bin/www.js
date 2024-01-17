41712fbf7865596ee0a2c0e62d76b56dc2db7ac0
chomihye <mihye0924@naver.com>
2024-01-17 12:34:33 +0900

    Initial commit

--- bin/www.js
+++ bin/www.js
@@ -1,8 +1,37 @@
 import app from '../app.js'; 
 import http from 'http'; 
-const port = process.env.PORT || '3000';
+const port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
  
-const server = http.createServer(app); 
-server.listen(port);
+const server = http.createServer(app);  
+  
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+server.listen(port); 
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
  

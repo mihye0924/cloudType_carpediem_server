bde0f635984eca7452e311cb1bbd2a779d020689
chomihye <mihye0924@naver.com>
2024-01-19 21:31:16 +0900

    2024.01.19 배포 테스트

--- routes/list.js
+++ routes/list.js
@@ -49,6 +49,14 @@ router.get('/:name', (req, res) => {
       conn.query(sql.listData, [account_name], function (error, results) { 
         if (error) throw error;
         if (results.length > 0) {   
+          
+          // results.forEach(( item ) => {    
+          // return data.push({
+          //     ...item,
+          //     list_image: JSON.stringify(item.list_image)
+          //   });
+          // })    
+          // console.log(data,"results")
           return res.send({ code: 200, result: results, message: 'List Profile is successfully' });
         } else {
           return res.send({ code: 401, message: 'List Profile is failed' });
